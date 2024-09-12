91f3f488b0debba46bf5f664a459280925a1202c
DSM2 <fatec-dsm2@academico.intra>
2024-09-12 20:28:03 -0300

    Update -frontend

--- SistemaLoja/index.js
+++ SistemaLoja/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-
+app.use(express.static('public'));
 const port = 8080;
 app.listen(port, (error) => {
   if (error) {
